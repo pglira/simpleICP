a47ede4b30e3348776cf1c329a7a71d243fd34cb
Philipp Glira <philipp.glira@ait.ac.at>
2020-05-19 11:35:36 +0200

    comment block removed

--- matlab/run_simpleicp.m
+++ matlab/run_simpleicp.m
@@ -7,12 +7,3 @@ XFix = dlmread(pathToPCFix);
 XMov = dlmread(pathToPCMov);
 
 H = simpleicp(XFix, XMov);
-
-% pcFix = pointcloud(XFix(:,1), XFix(:,2), XFix(:,3));
-% pcMov = pointcloud(XMov(:,1), XMov(:,2), XMov(:,3));
-% pcMov.transform(H);
-
-% pcview([pcFix.x pcFix.y pcFix.z]);
-% pcview([pcMov.x pcMov.y pcMov.z]);
-
-
