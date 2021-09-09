e46d5eeaf564e82532ae837e605d4c5183793b32
Philipp Glira <philipp.glira@ait.ac.at>
2021-09-09 14:01:48 +0200

    Added test code and sections

--- matlab/run_simpleicp.m
+++ matlab/run_simpleicp.m
@@ -1,15 +1,30 @@
 clc; clear; close;
 
-pathToPCFix = "../data/dragon1.xyz";
-pathToPCMov = "../data/dragon2.xyz";
+%% Dataset "Dragon"
+XFix = dlmread("../data/dragon1.xyz");
+XMov = dlmread("../data/dragon2.xyz");
+[H, XMovT] = simpleicp(XFix, XMov);
 
-% pathToPCFix = "../data/airborne_lidar1.xyz";
-% pathToPCMov = "../data/airborne_lidar2.xyz";
+%% Dataset "Airborne Lidar"
+XFix = dlmread("../data/airborne_lidar1.xyz");
+XMov = dlmread("../data/airborne_lidar2.xyz");
+[H, XMovT] = simpleicp(XFix, XMov);
 
-% pathToPCFix = "../data/terrestrial_lidar1.xyz";
-% pathToPCMov = "../data/terrestrial_lidar2.xyz";
+%% Dataset "Terrestrial Lidar"
+XFix = dlmread("../data/terrestrial_lidar1.xyz");
+XMov = dlmread("../data/terrestrial_lidar2.xyz");
+[H, XMovT] = simpleicp(XFix, XMov);
 
-XFix = dlmread(pathToPCFix);
-XMov = dlmread(pathToPCMov);
+%% Dataset "Lion"
+XFix = dlmread("../data/lionscan1.xyz");
+XMov = dlmread("../data/lionscan2.xyz");
+[H, XMovT] = simpleicp(XFix, XMov, 'maxOverlapDistance', 10);
 
-H = simpleicp(XFix, XMov);
+%% Check
+addrepo('Point_cloud_tools_for_Matlab'); % https://github.com/pglira/Point_cloud_tools_for_Matlab
+pcFix = pointCloud(XFix, 'Label', 'XFix');
+pcMov = pointCloud(XMov, 'Label', 'XMov');
+pcMovT = pointCloud(XMovT, 'Label', 'XMovT');
+pcFix.plot('Color', 'r');
+pcMov.plot('Color', 'b');
+pcMovT.plot('Color', 'm');
