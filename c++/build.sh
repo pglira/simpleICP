46e5a106776a560c0e3431c57cd67300f00ce512
Philipp Glira <philipp.glira@ait.ac.at>
2022-09-14 07:11:26 +0000

    Update to latest c++ devcontainer, use vcpkg for deps

--- c++/build.sh
+++ c++/build.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
 
 mkdir -p build
 [ ! -d "build" ] && exit 1
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/usr/local/vcpkg/scripts/buildsystems/vcpkg.cmake
 make -j 4
